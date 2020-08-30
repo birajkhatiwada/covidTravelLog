f1b39c9b79ab2b76e4688050ee43e5a796be4fd8
Biraj <khatiwadabizzz@gmail.com>
2020-08-30 05:30:00 -0700

    geolocate and geocoder functions properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css"
+import "mapbox-gl/dist/mapbox-gl.css"
+import DeckGL, { GeoJsonLayer } from "deck.gl";
+import Geocoder from "react-map-gl-geocoder";
 import React, {useState, useEffect} from 'react';
-import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import ReactMapGL, {Marker, Popup, GeolocateControl} from 'react-map-gl';
 import {listLogEntries, listCovidEntries} from './API';
 import LogEntryForm from './LogEntryForm'
 
 
+const geolocateStyle = {
+  float: 'left',
+  margin: '50px',
+  padding: '10px'
+};
+
+
 const App = () =>{
+  const [mouseEntered, setMouseEntered] = useState(false);
   const [logEntries, setLogEntries] = useState([]);
   const [covidEntries, setCovidEntries] = useState([]);
   const [mapStyle, setMapStyle] = useState("mapbox://styles/birajkhatiwada/ckd6zh2zu0g251ikokpwdhpee");
   const [showPopup, setShowPopup] = useState({});
   const [showCovidPopup, setShowCovidPopup] = useState({});
   const [addEntryLocation, setAddEntryLocation] = useState(null);
+  const [searchResultLayer, setSearchResultLayer] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -19,7 +32,9 @@ const App = () =>{
     zoom: 3
   });
 
+  // const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 3000 })
 
+  const mapRef = React.useRef();
 
   const getEntries = async()=>{
     const logEntries = await listLogEntries();
@@ -32,6 +47,7 @@ const App = () =>{
     console.log(covidentries);
     setCovidEntries(covidentries);
   }
+
 
 
   useEffect(()=>{
@@ -48,11 +64,22 @@ const App = () =>{
     })
   }
 
+  const handleOnResult = (event) => {
+   setSearchResultLayer(new GeoJsonLayer({
+      id: "search-result",
+      data: event.result.geometry,
+      getFillColor: [255, 0, 0, 128],
+      getRadius: 1000,
+      pointRadiusMinPixels: 10,
+      pointRadiusMaxPixels: 10
+   }))
+  }
 
   return (
     <>
     <div className = "map">
     <ReactMapGL
+      ref = {mapRef}
       {...viewport}
       mapStyle = {mapStyle}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
@@ -68,13 +95,15 @@ const App = () =>{
             // offsetLeft={-12}
             // offsetTop={-24}
             >
-          <div
-            onClick={() => setShowPopup({
-              // ...showPopup,
-              [entry._id]: true,
-            })}
-              >
+          <div>
               <svg
+                onMouseOver={()=> setShowPopup({
+                  [entry._id]: true,
+                })}
+                onMouseOut={()=>setShowPopup({
+                  [entry._id]: false,
+                })}
+
                 className="marker"
                 style={{
                   height: `${9 * viewport.zoom}px`,
@@ -92,13 +121,15 @@ const App = () =>{
 
         {   showPopup[entry._id]?(
             <Popup
+              className='Popup'
               latitude={entry.latitude}
               longitude={entry.longitude}
               closeButton={true}
               closeOnClick={false}
               dynamicPosition={true}
               onClose={() => setShowPopup({})}
-              anchor="top" >
+              anchor="top"
+               >
               <div className='popup'>
                 <h3>{entry.title}</h3>
                 {/* <p>{entry.comments}</p> */}
@@ -126,19 +157,26 @@ const App = () =>{
             // offsetTop={-24}
             >
           <div
-            onClick={() => setShowCovidPopup({
-              // ...showPopup,
-              [index]: true,
-            })}
+
+            // onClick={() => setShowCovidPopup({
+            //   // ...showPopup,
+            //   [index]: true,
+            // })}
               >
               <svg
+                  onMouseOver={()=> setShowCovidPopup({
+                    [index]: true,
+                  })}
+                  onMouseOut={()=>setShowCovidPopup({
+                    [index]: false,
+                  })}
                  className="marker covid"
                  style={{
                    height: `${9 * viewport.zoom}px`,
                    width: `${9 * viewport.zoom}px`,
                  }}
                 id="Capa_1" enable-background="new 0 0 511 511" height="512" viewBox="0 0 511 511" width="512">
-                <g><circle cx="255.5" cy="225" r="136"/>
+                <g><circle className="circle" cx="255.5" cy="225" r="136"/>
                 <path d="m415.306 66.193c-42.685-42.685-99.439-66.193-159.806-66.193s-117.121 23.508-159.806 66.193c-42.686 42.687-66.194 99.44-66.194 159.807 0 106.499 74.454 198.443 177.887 220.849l48.113 64.151 48.114-64.152c103.432-22.406 177.886-114.349 177.886-220.848 0-60.367-23.508-117.12-66.194-159.807zm-159.806-7.193c91.533 0 166 74.468 166 166s-74.467 166-166 166-166-74.468-166-166 74.467-166 166-166z"/>
                 </g>
               </svg>
@@ -147,13 +185,16 @@ const App = () =>{
 
         {showCovidPopup[index]?(
             <Popup
+              className='Popup'
               latitude={entry.latitude}
               longitude={entry.longitude}
               closeButton={true}
               closeOnClick={false}
               dynamicPosition={true}
+              onMouseLeave = {() => setShowCovidPopup({})}
               onClose={() => setShowCovidPopup({})}
-              anchor="top" >
+              anchor="top"
+              >
               <div className='popup'>
                 <h3>{entry.location}</h3>
                 <p>Confirmed: {entry.confirmed}</p>
@@ -202,6 +243,7 @@ const App = () =>{
               closeButton={true}
               closeOnClick={false}
               dynamicPosition={true}
+              onMouseLeave = {() => setShowCovidPopup({})}
               onClose={() => setAddEntryLocation(null)}
               anchor="top" >
               <div className='popup'>
@@ -215,16 +257,40 @@ const App = () =>{
         </>
       ):null
     }
-    </ReactMapGL>
+
+    <GeolocateControl
+      className = "geoControl"
+      // style = {geolocateStyle}
+      positionOptions = {{enableHighAccuracy: true}}
+      trackUserLocation = {true}
+    />
+    <Geocoder
+                className = "geocoder"
+                mapRef={mapRef}
+                onResult={handleOnResult}
+                onViewportChange={setViewport}
+                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+                // mapboxApiAccessToken={"mapbox://styles/birajkhatiwada/ckd6zcsfn0g031ise8iy1qhm6"}
+                position='top-right'
+              />
+
+    <div className="toggle-btns">
+      <button className="toggle" onClick={()=> setMapStyle("mapbox://styles/birajkhatiwada/ckd6zcsfn0g031ise8iy1qhm6")}>Basic Map</button>
+      <button className="toggle" onClick={()=>setMapStyle("mapbox://styles/birajkhatiwada/ckd6zh2zu0g251ikokpwdhpee")}>Satellite Map</button>
+      <button className="toggle" onClick={()=>setMapStyle("mapbox://styles/birajkhatiwada/ckd9v0k840kwa1imo7uy2yui7")}>Outdoor Map</button>
     </div>
-    <br/>
-    <nav>
+    </ReactMapGL>
+    {/* <DeckGL {...viewport} layers={[searchResultLayer]} /> */}
+
+    </div>
+    {/* <br/> */}
+    {/* <nav>
     <div className="btns">
       <button className="toggle" onClick={()=> setMapStyle("mapbox://styles/birajkhatiwada/ckd6zcsfn0g031ise8iy1qhm6")}>Basic Map</button>
       <button className="toggle" onClick={()=>setMapStyle("mapbox://styles/birajkhatiwada/ckd6zh2zu0g251ikokpwdhpee")}>Satellite Map</button>
       <button className="toggle" onClick={()=>setMapStyle("mapbox://styles/birajkhatiwada/ckd9v0k840kwa1imo7uy2yui7")}>Outdoor Map</button>
     </div>
-    </nav>
+    </nav> */}
     </>
   );
 }
